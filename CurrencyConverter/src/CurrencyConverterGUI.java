41cd7a9b1e18d115a062daa570eeecbedefa9a64
joywin2003 <21a20.joywin@sjec.ac.in>
2023-03-06 00:30:38 +0530

    Added actionPerformed class

--- CurrencyConverter/src/CurrencyConverterGUI.java
+++ CurrencyConverter/src/CurrencyConverterGUI.java
@@ -10,24 +10,24 @@ abstract class CurrencyConverter {
 
     abstract void setRate(double rate);
 
-    abstract double convert(double amount) ;
+    abstract double convert(double amount,String fromCurrency,String toCurrency) ;
 
-//    abstract String getCurrencyName();
+
 }
 
-public class CurrencyConverterGUI extends JFrame implements ActionListener{
+public class CurrencyConverterGUI extends JFrame implements ActionListener {
     private static final String API_KEY = "b84ec73e3c8d5bc6bb471a88";
     private static final String API_ENDPOINT = "https://v6.exchangerate-api.com/v6/b84ec73e3c8d5bc6bb471a88/latest/USD";
     public static String[] CURRENCIES = {"USD", "EUR", "GBP", "JPY", "CAD", "AUD", "NZD", "CHF", "HKD", "SGD"};
     public static final double[] RATES = new double[CURRENCIES.length];
 
     public static JFrame f;
-    public static JLabel amountLabel,fromLabel,toLabel,lastUpdatedLabel;
-    public static JComboBox<String> fromCurrencyBox,toCurrencyBox;
+    public static JLabel amountLabel, fromLabel, toLabel, resultLabel;
+    public static JComboBox<String> fromCurrencyBox, toCurrencyBox;
     public static JTextField AmountField;
     public static JPanel p;
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         f = new JFrame("CURENCIO");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(300, 350);
@@ -35,15 +35,18 @@ public class CurrencyConverterGUI extends JFrame implements ActionListener{
         f.setLocationRelativeTo(null);
 
         amountLabel = new JLabel("Amount");
+        amountLabel.setFont(new Font("Arial", Font.BOLD, 16));
         amountLabel.setBounds(15, 10, 100, 34);
 
         AmountField = new JTextField("$1");
         AmountField.setBounds(10, 40, 180, 34);
 
         fromLabel = new JLabel("From");
+        fromLabel.setFont(new Font("Arial", Font.BOLD, 16));
         fromLabel.setBounds(15, 70, 80, 34);
 
         toLabel = new JLabel("To");
+        toLabel.setFont(new Font("Arial", Font.BOLD, 16));
         toLabel.setBounds(15, 130, 80, 34);
 
         fromCurrencyBox = new JComboBox<>(CURRENCIES);
@@ -58,7 +61,8 @@ public class CurrencyConverterGUI extends JFrame implements ActionListener{
         JButton converterButton = new JButton("Convert");
         converterButton.setBackground(Color.RED);
         converterButton.setBounds(10, 220, 280, 40);
-
+        CurrencyConverterGUI cg = new CurrencyConverterGUI();
+        converterButton.addActionListener(cg);
         f.add(amountLabel);
         f.add(AmountField);
         f.add(fromLabel);
@@ -69,18 +73,33 @@ public class CurrencyConverterGUI extends JFrame implements ActionListener{
         f.add(p);
         f.setVisible(true);
     }
-    public void actionPerformed(ActionEvent e) {
-        // code to handle button click event goes here
-
+    void setRate(double rate) {
     }
-    public static class ExchangeRate extends CurrencyConverter{
-        @Override
-        void setRate(double rate) {}
 
-        @Override
-        double convert(double amount) {
-            return 0;
+    public double convert(double amount,String fromCurrency,String toCurrency){
+        double usd = 1, ind = 83, rateTo = 20;
+        double rateFrom = usd / ind;
+        double result = rateFrom * rateTo;
+        return result;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        if (e.getSource() == s) {
+            double amount = Double.parseDouble(AmountField.getText());
+            System.out.println(amount);
+            String fromCurrency = (String) fromCurrencyBox.getSelectedItem();
+            String toCurrency = (String) toCurrencyBox.getSelectedItem();
+            double result = convert(amount,fromCurrency,toCurrency);
+
+            resultLabel = new JLabel(String.format("%.2f %s = %.2f %s", amount, fromCurrency, result, toCurrency));
+            resultLabel.setFont(new Font("Arial", Font.BOLD, 16));
+            resultLabel.setForeground(Color.BLUE);
+            resultLabel.setBounds(10, 200, 280, 20);
+            f.add(resultLabel);
+            f.revalidate();
         }
+
     }
 }
 
